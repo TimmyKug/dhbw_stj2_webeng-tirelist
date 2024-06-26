edf642627aa690c510b63401179d08a4195965c2
BuildTools <unconfigured@null.spigotmc.org>
2024-06-26 15:09:38 +0200

    test api

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    await fetch('https://lukas.rip/api/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "group-key": GROUP_KEY,
+        },
+        body: JSON.stringify({
+            username: "Ncio",
+            password: "Ncio123",
+            profile: {
+                displayName: "Ncioo",
+                description: "Ncio ist schön",
+            }
+        })
+    })
     const users = await fetch('https://lukas.rip/api/users', {
         method: 'GET',
         headers: {
-            "group-key": "2ujgh9kh"
+            "group-key": GROUP_KEY
         }
     })
     console.log(await users.json());
 
 })
+
+
+
+const GROUP_KEY = '2ujgh9kh'
